e31576282e3589c56963edd7ecf015da62c2334c
weslley <weslleyhariellima@gmail.com>
2025-03-20 10:50:26 -0400

    INSERÇÃO DE HASH,SIMMBOLOS E REPETICOES

---
+++ ruby/6.hash/hash.rb
@@ -0,0 +1,19 @@
+#CRIAÇÃO
+#hash = { nome: "wes", idade: 27 }
+#puts hash
+
+
+#BUSCA ESPECIFICA
+#hash = { nome: "wes", idade: 27 }
+#puts hash[:nome]
+
+#ADICIONANDO COISAS AO HASH
+#hash = { nome: "wes", idade: 27 }
+#hash[:altura]=1.92
+#puts hash
+
+#COMPARAR
+#pessoa1 = { :um => 1,:dois=> 2}
+#pessoa2={:um => 1,:dois=> 2}
+#puts pessoa1 == pessoa2
+
---
+++ ruby/7.Repetições (For, While, Times, Loop)/for.rb
@@ -0,0 +1,61 @@
+# Lista de nomes REPETICAO FOR
+#nomes = ["João", "Maria", "José", "Mateus"]
+#
+#dict = {nome: "Diego", idade:35, altura:1.78}
+#
+#for nome in nomes do 
+#  puts nome
+#end
+#
+#WHILE
+# Lista de nomes
+#nomes = ["João", "Maria", "José", "Mateus"]
+
+# Inicializa o contador
+#count = 0
+
+# Loop while para percorrer a lista de nomes
+#while count < nomes.size do
+#  # Imprime o nome na posição 'count'
+#  puts nomes[count]
+#  
+#  # Imprime o valor do contador
+#  puts count
+#  
+#  # Incrementa o contador
+#  count += 1
+#end
+
+#TIMES
+## Inicializa o contador 'c' com 1
+#c = 1
+## Repete o bloco de código 10 vezes
+#10.times do
+#  # Imprime a string "Diego"
+#  puts "Diego"
+#  
+#  # Imprime o valor atual do contador 'c'
+#  puts c
+#  
+#  # Incrementa o contador 'c' em 1
+#  c += 1
+#end
+
+#WHILE DO 
+## Inicializa o contador 'count' com 1
+#count = 1
+#
+## Inicia um loop infinito
+#loop do
+#  # Imprime a string "Diego"
+#  puts "Diego"
+#  
+#  # Verifica se o contador 'count' chegou a 10
+#  if count == 10
+#    # Se sim, sai do loop
+#    break
+#  end
+#  
+#  # Incrementa o contador 'count' em 1
+#  count += 1
+#end
